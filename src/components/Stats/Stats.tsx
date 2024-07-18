8dca3f5ffe6a2bf9a59411e24da06fa36ebb25ff
Suga <okamisuga@gmail.com>
2024-07-18 17:40:43 +0200

    Adds Wounds section

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -5,18 +5,41 @@ const Stats = () => {
     return (
         <div className="Stats">
             <div className='player-health'>
-                <div>WOUNDS</div>
+                <div className='player-wounds'>
+                    <span>WOUNDS</span>
+                    <input type="text" maxLength={2} />
+                    <div className='wounds-table'>
+                        <span>SB</span>
+                        <span>TB+2</span>
+                        <span>WPB</span>
+                        <span>Hardy</span>
+                        <span>Max.</span>
+                        <input type="text" maxLength={2} />
+                        <input type="text" maxLength={2} />
+                        <input type="text" maxLength={2} />
+                        <input type="text" maxLength={2} />
+                        <input type="text" maxLength={2} />
+                    </div>
+                </div>
                 <div>CRITICAL WOUNDS</div>
             </div>
 
             <div className='player-points'>
-                <div>Fate</div>
-                <div>Fortune</div>
+                <div>FATE</div>
+                <div>FORTUNE</div>
                 <div>RESILIENCE</div>
                 <div>RESOLVE</div>
             </div>
 
+
             <div className='player-status'>
+                <div className='status-conditions'>
+                    <span>CONDITIONS</span>
+                    <div>
+                        <Banner />
+                        <Banner />
+                    </div>
+                </div>
                 <div className='status-psychology'>
                     <span>PSYCHOLOGY</span>
                     <div>
@@ -30,12 +53,17 @@ const Stats = () => {
                         <Banner />
                         <Banner />
                         <Banner />
+                    </div>
+                </div>
+                <div className='status-corruption'>
+                    <span>CORRUPTION & MUTATIONS</span>
+                    <div>
+                        <Banner />
+                        <Banner />
                         <Banner />
                         <Banner />
                     </div>
                 </div>
-                <div>Corruption & Mutations</div>
-                <div>Conditions</div>
             </div>
         </div>
     )
