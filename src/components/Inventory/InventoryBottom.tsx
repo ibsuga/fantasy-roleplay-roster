f2cf19e0972f8d3f6417dbfcff493a52279aa00f
Suga <okamisuga@gmail.com>
2024-10-14 14:44:30 +0200

    Adds inventory bottom tooltips and styles

--- src/components/Inventory/InventoryBottom.tsx
+++ src/components/Inventory/InventoryBottom.tsx
@@ -1,9 +1,9 @@
 import useItemStore from "../../store/InventoryStore";
+import PopMenu from '../PopMenu/PopMenu';
 import { useMemo } from "react";
-import { BiSolidCoin } from "react-icons/bi";
+import { GiTwoCoins, GiSwapBag } from 'react-icons/gi';
 import { FaWeightHanging } from "react-icons/fa";
 import { TbInfoHexagon } from "react-icons/tb";
-import PopMenu from '../PopMenu/PopMenu';
 
 
 
@@ -27,8 +27,8 @@ const InventoryBottom = () => {
     const totalEncumbrance = useMemo(() => items.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
 
 
-    const EncumbranceTooltipContent =
-        <div className="encumbrance-tooltip">
+    const EncumbranceInfoTooltipContent =
+        <div className="encumbrance-info-tooltip">
             <span className="header">Encumbrance by Category</span>
             <span className="header-background">Encumbrance</span>
             <hr />
@@ -47,15 +47,65 @@ const InventoryBottom = () => {
         </div>
 
 
+    const EncumbranceRulesTooltipContent =
+        <div className="encumbrance-rules-tooltip">
+            <div className="header">ENCUMBRANCE RULES</div>
+            <span className="header-background">Rules</span>
+            <hr />
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Encumbrance</th>
+                        <th>Penalty</th>
+                    </tr>
+                    <tr>
+                        <td>Up to limit</td>
+                        <td>No penalties.</td>
+                    </tr>
+                    <tr>
+                        <td>2x limit</td>
+                        <td>-1 Movement (min:3), -10 Agi , +1 Travel Fatigue.</td>
+                    </tr>
+                    <tr>
+                        <td>3x limit</td>
+                        <td>-2 Movement (min:2), -20 Agi (min:10), +1 Travel Fatigue.</td>
+                    </tr>
+                    <tr>
+                        <td>More than 3x</td>
+                        <td>You're not moving.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
-    console.log(wealth);
+
+    const WealthConversionTooltipContent =
+        <div className="conversion-tooltip">
+            <div className="header">COIN CONVERSION</div>
+            <div className="header-background">Conversion</div>
+            <hr />
+            <div className="content">
+                <span>Standard coin values are:</span>
+                <span> <GiTwoCoins className="gold" /> 1 gold crown (1GC) = <GiTwoCoins className="silver" /> 20 silver shillings (20/–) </span>
+                <span><GiTwoCoins className="silver" /> 1 silver shilling (1/–) = <GiTwoCoins className="copper" /> 12 brass pennies (12d)</span>
+
+            </div>
+
+        </div>
+
 
     return (
         <div className='inventory-bottom'>
 
             <div className='encumbrance'>
-                <FaWeightHanging />
-                <div className='encumbrance-table'>
+                <PopMenu
+                    className="encumbrance-rules-tooltip"
+                    label={<FaWeightHanging className="rules-icon" />}
+                    content={EncumbranceRulesTooltipContent}
+                    positions={['top']}
+                    align="end"
+                />
+                <div className='encumbrance-section'>
                     <span className='label'>Encumbrance</span>
                     <span className={(totalEncumbrance > encumbrance) ? "encumbered" : 'not-encumbered'} >{totalEncumbrance}</span>
                     <span className='label'>Max.</span>
@@ -63,10 +113,11 @@ const InventoryBottom = () => {
                         type="text"
                         maxLength={3}
                         value={encumbrance}
-                        onChange={(e) => updateMaxEncumbrance(Number(e.target.value) || 0)} />
+                        onChange={(e) => updateMaxEncumbrance(Number(e.target.value) || 0)}
+                    />
                     <PopMenu
                         label={<TbInfoHexagon className="info-button" />}
-                        content={EncumbranceTooltipContent}
+                        content={EncumbranceInfoTooltipContent}
                         positions={['top']}
                         align="end"
                     />
@@ -75,19 +126,29 @@ const InventoryBottom = () => {
             </div>
 
             <div className='wealth'>
-                <div className="coin-copper">
-                    <BiSolidCoin />
-                    <input type="text" value={wealth.copper} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'copper')} />
-                </div>
-                <div className="coin-silver">
-                    <BiSolidCoin />
-                    <input type="text" value={wealth.silver} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'silver')} />
-                </div>
-                <div className="coin-gold">
-                    <BiSolidCoin />
-                    <input type="text" value={wealth.gold} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'gold')} />
+                <PopMenu
+                    className="conversion-tooltip"
+                    label={<GiSwapBag className="conversion-icon" />}
+                    content={WealthConversionTooltipContent}
+                    positions={['top']}
+                    align="end"
+                />
+                <div className="coin-section">
+                    <div className="copper">
+                        <GiTwoCoins />
+                        <input type="text" value={wealth.copper} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'copper')} />
+                    </div>
+                    <div className="silver">
+                        <GiTwoCoins />
+                        <input type="text" value={wealth.silver} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'silver')} />
+                    </div>
+                    <div className="gold">
+                        <GiTwoCoins />
+                        <input type="text" value={wealth.gold} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'gold')} />
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
