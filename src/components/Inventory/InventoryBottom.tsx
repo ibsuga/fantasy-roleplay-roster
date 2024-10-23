6c49e56472346efc2f355b8415b28ac51ca8dfcf
Manel Escandell <middengard@gmail.com>
2024-10-23 15:25:00 +0200

    Code clean up and refactor

--- src/components/Inventory/InventoryBottom.tsx
+++ src/components/Inventory/InventoryBottom.tsx
@@ -6,150 +6,146 @@ import { FaWeightHanging } from "react-icons/fa";
 import { TbInfoHexagon } from "react-icons/tb";
 
 
-
-
 const InventoryBottom = () => {
-    const items = useItemStore((state) => state.items);
-    const encumbrance = useItemStore((state) => state.encumbrance);
-    const updateMaxEncumbrance = useItemStore((state) => state.updateMaxEncumbrance);
+  const [items, encumbrance, updateMaxEncumbrance, wealth, updateWealth] = useItemStore((state) => [
+    state.items,
+    state.encumbrance,
+    state.updateMaxEncumbrance,
+    state.wealth,
+    state.updateWealth
+  ]);
 
-    const wealth = useItemStore((state) => state.wealth);
-    const updateWealth = useItemStore((state) => state.updateWealth)
+  //Encumbrance filters by category
+  const weaponItems = useMemo(() => items.filter((item) => item.category === 'weapon'), [items]);
+  const armorItems = useMemo(() => items.filter((item) => item.category === 'armor'), [items]);
+  const consumableItems = useMemo(() => items.filter((item) => item.category === 'items'), [items]);
 
-    //Encumbrance filters by category
-    const weaponEncumbranceFilter = useMemo(() => items.filter((item) => item.category === 'weapon'), [items]);
-    const armorEncumbranceFilter = useMemo(() => items.filter((item) => item.category === 'armor'), [items]);
-    const consumableEncumbranceFilter = useMemo(() => items.filter((item) => item.category === 'items'), [items]);
-
-    const weaponEncumbrance = useMemo(() => weaponEncumbranceFilter.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items])
-    const armorEncumbrance = useMemo(() => armorEncumbranceFilter.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
-    const consumableEncumbrance = useMemo(() => consumableEncumbranceFilter.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
-    const totalEncumbrance = useMemo(() => items.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
+  const weaponEncumbrance = useMemo(() => weaponItems.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
+  const armorEncumbrance = useMemo(() => armorItems.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
+  const consumableEncumbrance = useMemo(() => consumableItems.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
+  const totalEncumbrance = useMemo(() => items.reduce((prev: number, item: any) => prev + (item.encumbrance * 1), 0), [items]);
 
 
-    const EncumbranceInfoTooltipContent =
-        <div className="encumbrance-info-tooltip">
-            <span className="header">Encumbrance by Category</span>
-            <span className="header-background">Encumbrance</span>
-            <hr />
-            <div>
-                <span className="label">Weapons: </span>
-                <span className="encumbrance-value">{weaponEncumbrance}</span>
-            </div>
-            <div>
-                <span className="label">Armor: </span>
-                <span className="encumbrance-value">{armorEncumbrance}</span>
-            </div>
-            <div>
-                <span className="label">Items: </span>
-                <span className="encumbrance-value">{consumableEncumbrance}</span>
-            </div>
+  const EncumbranceInfoTooltipContent =
+    <div className="encumbrance-info-tooltip">
+      <span className="header">Encumbrance by Category</span>
+      <span className="header-background">Encumbrance</span>
+      <hr />
+      <div>
+        <span className="label">Weapons: </span>
+        <span className="encumbrance-value">{weaponEncumbrance}</span>
+      </div>
+      <div>
+        <span className="label">Armor: </span>
+        <span className="encumbrance-value">{armorEncumbrance}</span>
+      </div>
+      <div>
+        <span className="label">Items: </span>
+        <span className="encumbrance-value">{consumableEncumbrance}</span>
+      </div>
+    </div>;
+
+
+  const EncumbranceRulesTooltipContent =
+    <div className="encumbrance-rules-tooltip">
+      <div className="header">ENCUMBRANCE RULES</div>
+      <span className="header-background">Rules</span>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <th>Encumbrance</th>
+            <th>Penalty</th>
+          </tr>
+          <tr>
+            <td>Up to limit</td>
+            <td>No penalties.</td>
+          </tr>
+          <tr>
+            <td>2x limit</td>
+            <td>-1 Movement (min:3), -10 Agi , +1 Travel Fatigue.</td>
+          </tr>
+          <tr>
+            <td>3x limit</td>
+            <td>-2 Movement (min:2), -20 Agi (min:10), +1 Travel Fatigue.</td>
+          </tr>
+          <tr>
+            <td>More than 3x</td>
+            <td>You're not moving.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>;
+
+
+  const WealthConversionTooltipContent =
+    <div className="conversion-tooltip">
+      <div className="header">COIN CONVERSION</div>
+      <div className="header-background">Conversion</div>
+      <hr />
+      <div className="content">
+        <span>Standard coin values are:</span>
+        <span> <GiTwoCoins className="gold" /> 1 gold crown (1GC) = <GiTwoCoins className="silver" /> 20 silver shillings (20/–) </span>
+        <span><GiTwoCoins className="silver" /> 1 silver shilling (1/–) = <GiTwoCoins className="copper" /> 12 brass pennies (12d)</span>
+      </div>
+    </div>;
+
+
+  return (
+    <div className='inventory-bottom'>
+
+      <div className='encumbrance'>
+        <PopMenu
+          trigger={<FaWeightHanging className="rules-icon" />}
+          content={EncumbranceRulesTooltipContent}
+          positions={['top']}
+          direction="top"
+        />
+        <div className='encumbrance-section'>
+          <span className='label'>Encumbrance</span>
+          <span className={(totalEncumbrance > encumbrance) ? "encumbered" : 'not-encumbered'} >{totalEncumbrance}</span>
+          <span className='label'>Max.</span>
+          <input
+            type="text"
+            maxLength={3}
+            value={encumbrance}
+            onChange={(e) => updateMaxEncumbrance(Number(e.target.value) || 0)}
+          />
+          <PopMenu
+            trigger={<TbInfoHexagon className="info-button" />}
+            content={EncumbranceInfoTooltipContent}
+            positions={['top']}
+            align='end'
+            direction="top"
+          />
         </div>
+      </div>
 
-
-    const EncumbranceRulesTooltipContent =
-        <div className="encumbrance-rules-tooltip">
-            <div className="header">ENCUMBRANCE RULES</div>
-            <span className="header-background">Rules</span>
-            <hr />
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Encumbrance</th>
-                        <th>Penalty</th>
-                    </tr>
-                    <tr>
-                        <td>Up to limit</td>
-                        <td>No penalties.</td>
-                    </tr>
-                    <tr>
-                        <td>2x limit</td>
-                        <td>-1 Movement (min:3), -10 Agi , +1 Travel Fatigue.</td>
-                    </tr>
-                    <tr>
-                        <td>3x limit</td>
-                        <td>-2 Movement (min:2), -20 Agi (min:10), +1 Travel Fatigue.</td>
-                    </tr>
-                    <tr>
-                        <td>More than 3x</td>
-                        <td>You're not moving.</td>
-                    </tr>
-                </tbody>
-            </table>
+      <div className='wealth'>
+        <PopMenu
+          trigger={<GiSwapBag className="conversion-icon" />}
+          content={WealthConversionTooltipContent}
+          positions={['top']}
+          direction="top"
+        />
+        <div className="coin-section">
+          <div className="copper">
+            <GiTwoCoins />
+            <input type="text" value={wealth.copper} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'copper')} />
+          </div>
+          <div className="silver">
+            <GiTwoCoins />
+            <input type="text" value={wealth.silver} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'silver')} />
+          </div>
+          <div className="gold">
+            <GiTwoCoins />
+            <input type="text" value={wealth.gold} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'gold')} />
+          </div>
         </div>
+      </div>
 
-
-    const WealthConversionTooltipContent =
-        <div className="conversion-tooltip">
-            <div className="header">COIN CONVERSION</div>
-            <div className="header-background">Conversion</div>
-            <hr />
-            <div className="content">
-                <span>Standard coin values are:</span>
-                <span> <GiTwoCoins className="gold" /> 1 gold crown (1GC) = <GiTwoCoins className="silver" /> 20 silver shillings (20/–) </span>
-                <span><GiTwoCoins className="silver" /> 1 silver shilling (1/–) = <GiTwoCoins className="copper" /> 12 brass pennies (12d)</span>
-
-            </div>
-
-        </div>
-
-
-    return (
-        <div className='inventory-bottom'>
-
-            <div className='encumbrance'>
-                <PopMenu
-                    trigger={<FaWeightHanging className="rules-icon" />}
-                    content={EncumbranceRulesTooltipContent}
-                    positions={['top']}
-                    direction="top"
-                />
-                <div className='encumbrance-section'>
-                    <span className='label'>Encumbrance</span>
-                    <span className={(totalEncumbrance > encumbrance) ? "encumbered" : 'not-encumbered'} >{totalEncumbrance}</span>
-                    <span className='label'>Max.</span>
-                    <input
-                        type="text"
-                        maxLength={3}
-                        value={encumbrance}
-                        onChange={(e) => updateMaxEncumbrance(Number(e.target.value) || 0)}
-                    />
-                    <PopMenu
-                        trigger={<TbInfoHexagon className="info-button" />}
-                        content={EncumbranceInfoTooltipContent}
-                        positions={['top']}
-                        align='end'
-                        direction="top"
-                    />
-
-                </div>
-            </div>
-
-            <div className='wealth'>
-                <PopMenu
-                    trigger={<GiSwapBag className="conversion-icon" />}
-                    content={WealthConversionTooltipContent}
-                    positions={['top']}
-                    direction="top"
-                />
-                <div className="coin-section">
-                    <div className="copper">
-                        <GiTwoCoins />
-                        <input type="text" value={wealth.copper} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'copper')} />
-                    </div>
-                    <div className="silver">
-                        <GiTwoCoins />
-                        <input type="text" value={wealth.silver} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'silver')} />
-                    </div>
-                    <div className="gold">
-                        <GiTwoCoins />
-                        <input type="text" value={wealth.gold} maxLength={4} onChange={(e) => updateWealth(Number(e.target.value), 'gold')} />
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default InventoryBottom;
--- src/components/InventoryItem/InventoryItem.tsx
+++ src/components/InventoryItem/InventoryItem.tsx
@@ -1,259 +1,116 @@
 import './InventoryItem.css';
-import EditItemButton from '../Inventory/EditItemButton';
+import EditItemDialog from '../Inventory/components/EditItemDialog';
 import { GiCrocSword, GiChestArmor, GiPotionBall, GiPocketBow, GiHandBag } from "react-icons/gi";
-import useItemStore from '../../store/InventoryStore';
+import useItemStore, { itemType } from '../../store/InventoryStore';
 import PopMenu from '../PopMenu/PopMenu';
 import ContainerSelector from './ContainerSelector';
 
 
 const InventoryItem = (props: {
-    id: number,
-    name: string,
-    encumbrance: number,
-    damage: { value: number, useSB: boolean },
-    range: string,
-    category: string,
-    subCategory: string[],
-    availability: string,
-    qualities?: { name: string, description: string }[],
-    flaws?: { name: string, description: string }[],
-    locations?: string[],
-    armourPoints?: number,
-    amount?: number,
-    carry?: number,
-    isRanged?: boolean,
-    container_id?: number,
-    description?: string,
-
+  data: itemType;
 }) => {
+  const [containers, updateItemAmount] = useItemStore((state) => [state.containers, state.updateItemAmount]);
+  const container = containers.find((container) => container.id === props.data.container_id);
 
-    const [containers, updateItemAmount] = useItemStore((state) => [state.containers, state.updateItemAmount])
-    const container = containers.find((container) => container.id === props.container_id);
+  //Sets item icon for each category.
+  const itemIcons: any = {
+    'weapon': props.data.isRanged ? <GiPocketBow /> : <GiCrocSword />,
+    'armor': <GiChestArmor />,
+    'items': <GiPotionBall />,
+  };
 
-
-
-    //Sets item icon for each category.
-    const itemIcons: any = {
-        'weapon': props.isRanged ? <GiPocketBow /> : <GiCrocSword />,
-        'armor': <GiChestArmor />,
-        'items': <GiPotionBall />,
+  //Sets item stats depending on item category.
+  const getItemStatsContent = () => {
+    switch (props.data.category) {
+      case 'weapon':
+        return (
+          <div className='item-stats'>
+            <div className="stat">
+              <div className="label">DMG</div>
+              <span>{props.data.damage?.useSB ? '+SB+' : ''}</span>
+              <span>{props.data.damage?.value}</span>
+            </div>
+            <div className="stat">
+              <div className="label">ENC</div>
+              <span>{props.data.encumbrance}</span>
+            </div>
+          </div>
+        );
+      case 'armor':
+        return (
+          <div className='item-stats'>
+            <div className='stat'>
+              <div className='label'>AP</div>
+              <span>{props.data.armourPoints}</span>
+            </div>
+            <div className='stat'>
+              <div className="label">ENC</div>
+              <span>{props.data.encumbrance}</span>
+            </div>
+          </div>
+        );
+      case 'items':
+        return (
+          <div className='item-stats'>
+            <div className='stat'>
+              <div className="label">ENC</div>
+              <span>{props.data.encumbrance}</span>
+            </div>
+          </div>
+        );
+      default:
+        return <span>No Content...</span>;
     }
+  };
 
-    //Sets item stats depending on item category.
-    const getItemStatsContent = () => {
-        switch (props.category) {
-            case 'weapon':
-                return <div className='item-stats'>
+  return (
+    <div className="InventoryItem" >
 
-                    <div className="stat">
-                        <div className="label">DMG</div>
-                        <span>{props.damage?.useSB ? '+SB+' : ''}</span>
-                        <span>{props.damage?.value}</span>
-                    </div>
+      <div className='item-equip'>
+        <div>{itemIcons[props.data.category]}</div>
+      </div>
 
-                    <div className="stat">
-                        <div className="label">ENC</div>
-                        <span>{props.encumbrance}</span>
-                    </div>
-
-                </div>
-            case 'armor':
-                return <div className='item-stats'>
-
-                    <div className='stat'>
-                        <div className='label'>AP</div>
-                        <span>{props.armourPoints}</span>
-                    </div>
-
-                    <div className='stat'>
-                        <div className="label">ENC</div>
-                        <span>{props.encumbrance}</span>
-                    </div>
-
-                </div>
-            case 'items':
-                return <div className='item-stats'>
-
-                    <div className='stat'>
-                        <div className="label">ENC</div>
-                        <span>{props.encumbrance}</span>
-                    </div>
-
-                </div>
-            default: return <span>No Content...</span>
-        }
-    }
-
-
-    return (
-        <div className="InventoryItem" >
-
-            <div className='item-equip'>
-                <div>
-                    {itemIcons[props.category]}
-                </div>
-            </div>
-
-            <EditItemButton id={props.id}>
-                <div className='item-content'>
-
-                    <div className="item-name">{props.name}</div>
-
-                    {getItemStatsContent()}
-
-                </div>
-            </EditItemButton>
-
-            <div className="item-container">
-                <PopMenu
-                    trigger={
-                        <GiHandBag style={container ? { color: `#${container?.color}` } : {}} />
-                    }
-                    content={
-                        <div>
-                            <ContainerSelector
-                                itemId={props.id}
-                                containerId={null}
-                                containerLabel='No container'
-                            />
-                            {
-                                containers.map((container, index) =>
-                                    <ContainerSelector
-                                        key={index}
-                                        itemId={props.id}
-                                        containerId={container.id}
-                                        containerLabel={container.label}
-                                    />)
-                            }
-                        </div>
-                    }
-
-                />
-            </div>
-            <div className="item-amount">x
-                {
-                    <input
-                        type='text'
-                        value={props.amount ?? 1}
-                        onChange={(e: any) => updateItemAmount(props.id, e.target.value)}
-                    />
-                }
-            </div>
+      <EditItemDialog id={props.data.id}>
+        <div className='item-content'>
+          <div className="item-name">{props.data.name}</div>
+          {getItemStatsContent()}
         </div>
+      </EditItemDialog>
 
-    )
+      <div className="item-container">
+        <PopMenu
+          trigger={<GiHandBag style={container ? { color: `#${container?.color}` } : {}} />}
+          content={
+            <>
+              <ContainerSelector
+                itemId={props.data.id}
+                containerId={null}
+                containerLabel='No container'
+              />
+              {
+                containers.map((container, index) =>
+                  <ContainerSelector
+                    key={index}
+                    itemId={props.data.id}
+                    containerId={container.id}
+                    containerLabel={container.label}
+                  />)
+              }
+            </>
+          }
+        />
+      </div>
 
-}
+      <div className="item-amount">x
+        <input
+          type='text'
+          value={props.data.amount ?? 1}
+          onChange={(e: any) => updateItemAmount(props.data.id, e.target.value)}
+        />
+      </div>
+    </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     return (
-//         <div className="InventoryItem">
-//             <div className="background-icon"> {itemBackgroundIcons[props.category]} </div>
-//             <div className='header'>
-//                 <div className='item-name'>{props.name}</div>
-
-//                 <div className='item-tools'>
-//                     <GoNote onClick={() => setShowDescription(!showDescription)} />
-//                     <EditItemButton id={props.id} />
-//                     <div className='delete-button'>
-//                         <FaRegWindowClose onClick={() => deleteItem(props.id)} />
-//                     </div>
-//                 </div>
-//             </div>
-
-//             <div className='content'>
-//                 {showDescription ?
-//                     <>
-//                         <div className='item-description'>
-//                             <textarea
-//                                 placeholder='Add item description.'
-//                                 value={props.description}
-//                                 spellCheck={false}
-//                                 rows={2}
-//                                 onChange={(e) => updateItemDescription(props.id, e.target.value)}></textarea>
-//                         </div>
-//                     </>
-//                     :
-//                     <>
-//                         <div className='traits'>
-//                             {props.qualities && props.qualities.length > 0 &&
-//                                 <span className='trait-qualities'>
-//                                     {
-//                                         props.qualities?.map((quality) => {
-//                                             return <span className='quality'>
-//                                                 <PopMenu
-//                                                     label={quality.name}
-//                                                     content={
-//                                                         <div className="quality-tooltip">
-//                                                             <div className="header">
-//                                                                 <span>Quality</span>
-//                                                                 <div>{quality.name}</div>
-//                                                                 <hr />
-//                                                             </div>
-//                                                             <div className="description">
-//                                                                 <div>{quality.description}</div>
-//                                                             </div>
-//                                                         </div>}
-
-//                                                     positions={['top']}
-//                                                     align="center"
-//                                                 />
-//                                             </span>
-//                                         })
-//                                     }
-//                                 </span>
-//                             }
-//                             {props.flaws && props.flaws.length > 0 &&
-//                                 <span className='trait-flaws'>
-//                                     {
-//                                         props.flaws && props.flaws?.length > 0 && props.flaws?.map((flaw) => {
-//                                             return <span className='flaw'>
-//                                                 <PopMenu
-//                                                     label={flaw.name}
-//                                                     content={
-//                                                         <div className="flaw-tooltip">
-//                                                             <div className="header">
-//                                                                 <span>Flaw</span>
-//                                                                 <div>{flaw.name}</div>
-//                                                                 <hr />
-//                                                             </div>
-//                                                             <div className="description">
-//                                                                 <div>{flaw.description}</div>
-//                                                             </div>
-//                                                         </div>
-//                                                     }
-//                                                     positions={['top']}
-//                                                     align="center"
-//                                                 />
-//                                             </span>
-//                                         })
-//                                     }
-//                                 </span>
-//                             }
-//                         </div>
-//                         {getItemStatsContent()}
-//                     </>
-//                 }
-//             </div>
-//         </div >
-//     )
-// }
+  );
+};
 
 export default InventoryItem;
